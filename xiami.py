44e800c4ec1932f2630a4a666b8860cc8cc6b27e
Yangjie Li <latyas@gmail.com>
2014-08-02 13:43:55 +0800

    增加虾米淘宝帐号登录接口
    今天虾米的免费vip到期了,发现老接口注册不了虾米帐号了,桑心坏了.被迫解决淘宝的接口. 还不确定这样的代码是不是稳定.

--- xiami.py
+++ xiami.py
@@ -4,6 +4,7 @@ import sys
 from urllib2 import unquote
 import json
 import time
+import re
 import logging
 import xmltodict
 import requests
@@ -20,6 +21,39 @@ logger.setLevel(logging.NOTSET)
 
 reload(sys)
 sys.setdefaultencoding('utf-8')
+
+
+class Xiamiexp(Exception):
+    """
+    异常类
+    """
+    def __init__(self, err):
+        super(Xiamiexp, self).__init__()
+        self.err = err
+
+    def __str__(self):
+        return self.err_type()
+
+    def __expr__(self):
+        return self.__str__()
+
+    def err_type(self):
+        """
+        xiami_account 虾米帐号密码错误
+
+        taobao_account 淘宝帐号密码错误
+        taobao_captcha 淘宝帐号验证码输入错误
+        """
+
+        if self.err == 'xiami_account':
+            return u'虾米帐号密码错误'
+        elif self.err == 'taobao_account':
+            return u'淘宝帐号密码输入错误'
+        elif self.err == 'taobao_captcha':
+            return u'淘宝验证码输入错误'
+        elif self.err == 'generic_user_err':
+            return u'Cookies 无效, 请确认用户名密码或者 cookies 的有效性.'
+
 
 
 class Utils:
@@ -52,7 +86,7 @@ class Utils:
     def check_username(func, *args):
         def _wrapper(*args):
             obj = args[0]
-            if not obj.username or not obj.password:
+            if (not obj.username or not obj.password) and not obj.taobao:
                 logger.debug('用户未登录')
                 raise Exception('用户未登录')
             else:
@@ -63,23 +97,102 @@ class Utils:
               'Referer': 'http://img.xiami.com/static/swf/seiya/player.swf?v=1394535902294'}
 
 
-class Xiami:
-
-    def __init__(self, username=None, password=None):
+class Xiamibase(object):
+    """基类定义了基本的get和post访问
+    """
+    def __init__(self):
         self.session = requests.session()
+        self.captcha_handler = self._captcha_handler
+        self.captcha_func = captcha.show
+
+    def _safe_get(self, *args, **kwargs):
+        while True:
+            try:
+                data = self.session.get(*args, **kwargs)
+            except Exception as e:
+                # 失败重试
+                logger.error('Exception in _safe_get:' + str(e))
+                continue
+
+            if 'regcheckcode.taobao.com' in data.content:
+                print 'Captcha needed.'
+                # 目前只是把整个requests.Response和session对象传给验证码处理函数, 函数需自行处理完后重试.
+                self.captcha_handler(self.session, data)
+                continue
+            return data
+
+    def _safe_post(self, *args, **kwargs):
+        while True:
+            try:
+                data = self.session.post(*args, **kwargs)
+            except Exception as e:
+                # 失败重试
+                logger.error('Exception in _safe_get:' + str(e))
+                continue
+
+            if 'regcheckcode.taobao.com' in data.content:
+                # 目前只是把整个requests.Response和session对象传给验证码处理函数, 函数需自行处理完后重试.
+                self.captcha_handler(self.session, data)
+                continue
+            return data
+
+    def _captcha_handler(self, *args, **kwargs):
+
+        session = args[0]
+        html = args[1].text
+        bs = BeautifulSoup(html)
+        captcha_url = bs.find('img')['src']
+        input_session_id = bs.find('input', {'name': 'sessionID'})['value']
+        input_apply = bs.find('input', {'name': 'apply'})['value']
+        input_referer = bs.find('input', {'name': 'referer'})['value']
+
+        header = {'Referer': input_referer,
+                  'User-Agent': 'Mozilla/5.0'}
+
+        captcha_data = session.get(captcha_url, headers=header).content
+
+        captcha.show(captcha_data)
+        code = raw_input('captcha >')
+
+        url = 'http://www.xiami.com/alisec/captcha/tmdgetv3.php'
+        data = {'code': code,
+                'sessionID': input_session_id,
+                'apply': input_apply,
+                'referer': input_referer}
+
+        ret = session.post(url, data=data, headers=header)
+
+
+
+class Xiami(Xiamibase):
+    """Xiami类定义了虾米的登录和操作"""
+    def __init__(self, username=None, password=None,
+                 taobao=False, cookies=None):
+        super(Xiami, self).__init__()
         self.account = {}
         self.hq = False
         self.username = username
         self.password = password
-        self.captcha_handler = self._captcha_handler
-        if username and password:
-            self.login(username, password)
-        else:
-            logger.debug('没有制定用户名或者密码, 未登录')
 
-    def login(self, username, password):
-        self.account['username'] = username
-        self.account['password'] = password
+        if not taobao and not cookies:
+            self.from_xiami(username, password)
+
+        if taobao:
+            self.from_taobao(username, password)
+
+        if cookies:
+            self.from_cookies(cookies)
+
+        try:
+            self._get_uid()
+        except:
+            raise Xiamiexp('generic_user_err')
+
+
+    def from_xiami(self, username, password):
+        """虾米帐号登录, 需要没有绑定淘宝帐号
+        该登录方式即将被虾米废弃
+        """
         header = {'user-agent': 'Mozilla/5.0'}
         login_url = 'https://login.xiami.com/member/login'
         data = {'email': username,
@@ -89,22 +202,84 @@ class Xiami:
                 }
 
         ret = self._safe_post(login_url, data=data, headers=header)
+        """
         try:
             self._get_uid()
         except:
             logger.debug('用户名或者密码错误, 视为没有登录')
-            self.username = None
-            self.password = None
-        # get uid
+        """
+
+    def from_taobao(self, username, password):
+        """淘宝帐号登录, username 为淘宝帐号, password为支付宝帐号
+        注意不要和 alipay 帐号弄混了
+        """
+        captcha = ''
+        url = 'https://passport.alipay.com/mini_login.htm?lang=&appName=xiami&appEntrance=taobao&cssLink=&styleType=vertical&bizParams=&notLoadSsoView=&notKeepLogin=&rnd=0.6477347570091512?lang=zh_cn&appName=xiami&appEntrance=taobao&cssLink=https%3A%2F%2Fh.alipayobjects.com%2Fstatic%2Fapplogin%2Fassets%2Flogin%2Fmini-login-form-min.css%3Fv%3D20140402&styleType=vertical&bizParams=&notLoadSsoView=true&notKeepLogin=true&rnd=0.9090916193090379'
+        bs = BeautifulSoup(self._safe_get(url).content)
+
+        while True:
+            data = {'loginId': username,
+                'password': password,
+                'appName': 'xiami',
+                'appEntrance': 'taobao',
+                'hsid': bs.find('input', {'name': 'hsid'})['value'],
+                'cid': bs.find('input', {'name': 'cid'})['value'],
+                'rdsToken': bs.find('input', {'name': 'rdsToken'})['value'],
+                'umidToken': bs.find('input', {'name': 'umidToken'})['value'],
+                '_csrf_token': bs.find('input', {'name': '_csrf_token'})['value'],
+                'checkCode': captcha}
+            logger.debug('taobao post data' + str(data))
+
+            ret = self._safe_post('https://passport.alipay.com/newlogin/login.do?fromSite=0',
+                         headers={
+                            'Referer':'https://passport.alipay.com/mini_login.htm',
+                            'User-agent': 'Mozilla/5.0'},
+                         data=data).content
+
+            jdata = json.loads(ret)
+
+            # 出错处理
+            if jdata['content']['status'] == -1:
+                logger.debug('error,' + str(jdata))
+                # 有时候会有莫名其妙的问题
+                if 'titleMsg' not in jdata['content']['data']:
+                    continue
+                err_msg = jdata['content']['data']['titleMsg']
+                if err_msg == u'请输入验证码' or err_msg == u'验证码错误，请重新输入':
+                    session_id = bs.find('input', {'name': 'cid'})['value']
+                    captcha_url = 'http://pin.aliyun.com/get_img?identity=passport.alipay.com&sessionID=%s' % session_id
+                    logger.debug('captcha url:' + captcha_url)
+                    self.captcha_func(self._safe_get(captcha_url, headers={'User-agent': 'Mozilla/5.0'}).content)
+                    captcha = raw_input('captcha> ')
+                    continue  # 重新提交一次
+                else:
+                    print '未知错误'
+                    raise Xiamiexp('unknown')
+
+            # 登录成功, 将 st 传递给虾米
+            st = jdata['content']['data']['st']
+            logger.debug('st=' + st)
+
+            ret = self._safe_get('http://www.xiami.com/accounts/back?st=' + st,
+                        headers={'Referer':'https://passport.alipay.com/mini_login.htm',
+                                 'User-agent': 'Mozilla/5.0'})
+
+            # 由此登录完成
+            return
+
+    def from_cookies(self, cookies):
+        """通过cookies登录, 需要提交 dict 形式的cookies"""
+        for k, v in cookies.items():
+            self.session.cookies.set(k, v)
 
     @Utils.check_username
     def _get_uid(self):
         url = 'http://www.xiami.com/vip/myvip'
         header = {'user-agent': 'Mozilla/5.0',
-                  'Referer': 'http://img.xiami.com/static/swf/seiya/player.swf?v=1394535902294'}
-        ret = self._safe_get(url, headers=header).text
-        bs = BeautifulSoup(ret)
-        user_id = bs.find('input', attrs={'id': 'user_id'}).get('value')
+                  'Referer': 'http://img.xiami.com/staticmethod/swf/seiya/player.swf?v=1394535902294'}
+        ret = self._safe_get(url, headers=header).content
+        user_id = re.findall('loginMemberUid = \'(\d+)\'', ret)[0]
+        logger.debug('_get_uid, uid=' + user_id)
         self.account['uid'] = user_id
 
     @Utils.check_username
@@ -244,32 +419,6 @@ class Xiami:
                 self.captcha_handler(self.session, data)
                 continue
             return data
-
-    def _captcha_handler(self, *args, **kwargs):
-
-        session = args[0]
-        html = args[1].text
-        bs = BeautifulSoup(html)
-        captcha_url = bs.find('img')['src']
-        input_session_id = bs.find('input', {'name':'sessionID'})['value']
-        input_apply = bs.find('input', {'name':'apply'})['value']
-        input_referer = bs.find('input', {'name':'referer'})['value']
-
-        header = {'Referer': input_referer,
-                  'User-Agent': 'Mozilla/5.0'}
-
-        captcha_data = session.get(captcha_url, headers=header).content
-
-        captcha.show(captcha_data)
-        code = raw_input('captcha >')
-
-        url = 'http://www.xiami.com/alisec/captcha/tmdgetv3.php'
-        data = {'code': code,
-                'sessionID': input_session_id,
-                'apply': input_apply,
-                'referer': input_referer}
-
-        ret = session.post(url, data=data, headers=header)
 
 
     def _lib_func(self, func, handler, uid=None, url=None):
